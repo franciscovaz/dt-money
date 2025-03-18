61118284c7b777b972351068d01c9fac91707574
franciscovaz <ftomasvaz@gmail.com>
2025-03-18 21:33:10 +0000

    - add default colors; - add default global styles; - there is no need to use types for styled-components at the moment;

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./assets/themes/default"
+import { GlobalStyle } from "./assets/global"
+
 export function App() {
   return (
-    <h1>DT Money to track my expenses!</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>DT Money to track my expenses!</h1>
+    </ThemeProvider>
   )
 }
