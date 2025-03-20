b36654baba50bff1689c8118952abbf116dae2bb
franciscovaz <ftomasvaz@gmail.com>
2025-03-20 22:12:54 +0000

    - add header content; - add base header styles;

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,13 @@
+import { HeaderContainer, HeaderContent } from "./styles";
+import imgLogo from "../../assets/dt-money-logo.svg";
+
 export function Header() {
     return (
-        <header> </header>
+       < HeaderContainer>
+            <HeaderContent> 
+                <img src={imgLogo} alt="" />
+                <button>New transaction</button>
+            </HeaderContent>       
+    </HeaderContainer>
     )
 }
