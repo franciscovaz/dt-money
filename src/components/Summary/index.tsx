6a639bda6934673dfd85fc606951396cf259258e
franciscovaz <ftomasvaz@gmail.com>
2025-03-25 22:56:35 +0000

    - add content and styles to summary; - with test and fixed values;

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,35 +1,35 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyEur } from 'phosphor-react'
-import { SummaryContainer } from './styles'
+import { SummaryCard, SummaryContainer } from './styles'
 
 export function Summary() {
   return (
     <SummaryContainer>
-      <div>
+      <SummaryCard>
         <header>
           <span>Received</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
         <strong>€3000.00</strong>
-      </div>
+      </SummaryCard>
 
-      <div>
+      <SummaryCard>
         <header>
           <span>Spendings</span>
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
         <strong>€3000.00</strong>
-      </div>
+      </SummaryCard>
 
-      <div>
+      <SummaryCard variant="green">
         <header>
           <span>Total</span>
           <CurrencyEur size={32} color="#fff" />
         </header>
 
         <strong>€3000.00</strong>
-      </div>
+      </SummaryCard>
     </SummaryContainer>
   )
 }
