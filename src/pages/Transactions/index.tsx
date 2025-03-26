7210de7b8b4a1b1928ad8cb470ce7dfcc999ac54
franciscovaz <ftomasvaz@gmail.com>
2025-03-26 22:33:20 +0000

    - add transactions table; - add styles; - improve labels with income and outcome;

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,31 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Site development</td>
+                            <td><PriceHighlight type="income">€ 6000</PriceHighlight></td>
+                            <td>Sale</td>
+                            <td>20/03/2025</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Dinner out</td>
+                            <td><PriceHighlight type="outcome">-€ 100</PriceHighlight></td>
+                            <td>Food</td>
+                            <td>26/03/2025</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
