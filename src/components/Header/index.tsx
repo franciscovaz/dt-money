1b068fc095eeeaaa68486c9128a85f3f0ba75b90
franciscovaz <ftomasvaz@gmail.com>
2025-04-01 19:48:57 +0100

    - Add dialog from radix, without styles;

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
+
 import imgLogo from "../../assets/dt-money-logo.svg";
 
 export function Header() {
@@ -6,7 +8,24 @@ export function Header() {
        < HeaderContainer>
             <HeaderContent> 
                 <img src={imgLogo} alt="" />
-                <NewTransactionButton>New transaction</NewTransactionButton>
+
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>    
+                        <NewTransactionButton>New transaction</NewTransactionButton>
+                    </Dialog.Trigger>
+                    
+                    <Dialog.Portal>
+                        <Dialog.Overlay />
+
+                        <Dialog.Content>
+                            <Dialog.Title>New transaction</Dialog.Title>
+
+                            <Dialog.Close />
+                        </Dialog.Content>
+                    </Dialog.Portal>
+                    
+                </Dialog.Root>
+                
             </HeaderContent>       
     </HeaderContainer>
     )
