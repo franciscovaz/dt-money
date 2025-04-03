c1c07ab640c271dbcd34045dfa12dedddd1a0b88
franciscovaz <ftomasvaz@gmail.com>
2025-04-03 23:01:17 +0100

    - apply lint to the code;

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,34 +1,42 @@
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
-import { SearchForm } from "./components/SearchForm";
-import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
-    return (
-        <div>
-            <Header />
-            <Summary />
+  return (
+    <div>
+      <Header />
+      <Summary />
 
-            <TransactionsContainer>
-                <SearchForm />
-                
-                <TransactionsTable>
-                    <tbody>
-                        <tr>
-                            <td width="50%">Site development</td>
-                            <td><PriceHighlight type="income">€ 6000</PriceHighlight></td>
-                            <td>Sale</td>
-                            <td>20/03/2025</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Dinner out</td>
-                            <td><PriceHighlight type="outcome">-€ 100</PriceHighlight></td>
-                            <td>Food</td>
-                            <td>26/03/2025</td>
-                        </tr>
-                    </tbody>
-                </TransactionsTable>
-            </TransactionsContainer>
-        </div>
-    )
+      <TransactionsContainer>
+        <SearchForm />
+
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Site development</td>
+              <td>
+                <PriceHighlight type="income">€ 6000</PriceHighlight>
+              </td>
+              <td>Sale</td>
+              <td>20/03/2025</td>
+            </tr>
+            <tr>
+              <td width="50%">Dinner out</td>
+              <td>
+                <PriceHighlight type="outcome">-€ 100</PriceHighlight>
+              </td>
+              <td>Food</td>
+              <td>26/03/2025</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
 }
