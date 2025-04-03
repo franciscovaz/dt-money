3e8dd07d95b1f28cf3f0b57dd980c004b6d0b31a
franciscovaz <ftomasvaz@gmail.com>
2025-04-03 22:09:23 +0100

    - alignments;

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,32 +1,30 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import * as Dialog from '@radix-ui/react-dialog';
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
 
-import imgLogo from "../../assets/dt-money-logo.svg";
+import imgLogo from '../../assets/dt-money-logo.svg'
 
 export function Header() {
-    return (
-       < HeaderContainer>
-            <HeaderContent> 
-                <img src={imgLogo} alt="" />
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={imgLogo} alt="" />
 
-                <Dialog.Root>
-                    <Dialog.Trigger asChild>    
-                        <NewTransactionButton>New transaction</NewTransactionButton>
-                    </Dialog.Trigger>
-                    
-                    <Dialog.Portal>
-                        <Dialog.Overlay />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>New transaction</NewTransactionButton>
+          </Dialog.Trigger>
 
-                        <Dialog.Content>
-                            <Dialog.Title>New transaction</Dialog.Title>
+          <Dialog.Portal>
+            <Dialog.Overlay />
 
-                            <Dialog.Close />
-                        </Dialog.Content>
-                    </Dialog.Portal>
-                    
-                </Dialog.Root>
-                
-            </HeaderContent>       
+            <Dialog.Content>
+              <Dialog.Title>New transaction</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
+      </HeaderContent>
     </HeaderContainer>
-    )
+  )
 }
